9f9ec8ff991373e641fd4dd9c203b6fc668ef8af
Arnold Daniels <arnold@adaniels.nl>
2011-12-20 15:24:55 +0100

    Added vanilla generator bundle

---
+++ src/Jasny/Bundle/VanillaBundle/Resources/skeleton/crud/actions/delete.php
@@ -0,0 +1,63 @@
+
+    /**
+     * Deletes a {{ entity }} entity.
+     *
+{% if 'annotation' == format %}
+     * @Route("/{id}/delete", name="{{ route_name_prefix }}_delete")
+     * @Method("post")
+{% endif %}
+     */
+    public function deleteAction($id)
+    {
+        $form = $this->createDeleteForm($id);
+        $request = $this->getRequest();
+
+        $form->bindRequest($request);
+
+        if ($form->isValid()) {
+{%if triggerable %}
+            $entity->onDelete($this->get('security.context')->getToken()->getUser());
+            
+{% endif %}
+            $em = $this->getDoctrine()->getEntityManager();
+            $entity = $em->getRepository('{{ bundle }}:{{ entity }}')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find {{ entity_desc.singular }}.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+            
+{% if stringable %}
+        $this->get('session')->setFlash('notice', "{{ "Deleted %s '$entity'"|trans({'%s': entity_desc.singular})|capitalize }}");
+{% else %}
+        $this->get('session')->setFlash('notice', "{{ "Deleted %s"|trans({'%s': entity_desc.singular})|capitalize }}");
+{% endif %}
+        }
+
+        return $this->redirect($this->generateUrl('{{ route_name_prefix }}'));
+    }
+
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+    
+{% if ('index' in actions) and ('show' not in actions) and ('edit' not in actions) %}
+    private function createDeleteTokens($entities)
+    {
+        $tokens = array();
+        
+        foreach ($entities as $entity) {
+            $fields = $this->createDeleteFrom($entity->getId())->getChildren();
+            $tokens[$entity->getId()] = $fields['_token']->getData();
+        }
+        
+        return $tokens;
+    }
+{% endif %}
+    
