67ebd68011398c357e3b90e611f5766dc7bf2e86
Arnold Daniels <arnold@adaniels.nl>
2012-02-17 20:39:28 +0100

    Right way to do form types Added DateTimeType Set placeholder and inputmask as var for DateType Use placeholder and inputmask vars in bootstrap form layout template

--- src/Jasny/Bundle/FrameworkBundle/Form/Type/DateType.php
+++ src/Jasny/Bundle/FrameworkBundle/Form/Type/DateType.php
@@ -11,7 +11,10 @@
 
 namespace Jasny\Bundle\FrameworkBundle\Form\Type;
 
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\Extension\Core\Type\DateType as BaseType;
 
 class DateType extends BaseType
@@ -22,22 +25,38 @@ class DateType extends BaseType
     public function buildForm(FormBuilder $builder, array $options)
     {
         if (isset($options['pattern'])) $options['format'] = $options['pattern'];
+
+        $pattern = is_string($options['format']) ? $options['format'] : datefmt_get_pattern(datefmt_create(\Locale::getDefault(), $options['format'], \IntlDateFormatter::NONE, \DateTimeZone::UTC, \IntlDateFormatter::GREGORIAN));
+        $pattern = strtolower(preg_replace(array('/\bd\b/', '/\bM\b/', '/\by{1,2}\b/', '/\by{3,}\b/'), array('dd', 'mm', 'yy', 'yyyy'), $options['format']));
+        
+        $builder->setAttribute('inputmask', $options['inputmask'] === true ? preg_replace('/\w/', '9', $pattern) : $options['inputmask']);
+        $builder->setAttribute('placeholder', $options['placeholder'] === true ? $pattern : $options['placeholder']);
+
         parent::buildForm($builder, $options);
     }
 
     /**
      * {@inheritdoc}
      */
+    public function buildView(FormView $view, FormInterface $form)
+    {
+        $view->set('inputmask', $form->getAttribute('inputmask'));
+        $view->set('placeholder', $form->getAttribute('placeholder'));
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
     public function getDefaultOptions(array $options)
     {
-        $date_pattern = preg_replace(array('/d+/', '/M+/', '/y+/'), array('dd', 'MM', 'yyyy'), datefmt_get_pattern(datefmt_create(\Locale::getDefault(), \IntlDateFormatter::SHORT, \IntlDateFormatter::NONE, \DateTimeZone::UTC, \IntlDateFormatter::GREGORIAN)));
-        $date_mask = preg_replace('/\w/', '9', $date_pattern);
+        $date_pattern = preg_replace('/\byy?\b/', 'yyyy', datefmt_get_pattern(datefmt_create(\Locale::getDefault(), \IntlDateFormatter::SHORT, \IntlDateFormatter::NONE, \DateTimeZone::UTC, \IntlDateFormatter::GREGORIAN)));
 
         return array(
-           'widget' => 'single_text',
-           'format' => \IntlDateFormatter::LONG,
-           'pattern' => $date_pattern,
-           'attr' => array('class' => 'datepicker', 'data-mask' => strtolower($date_mask), 'placeholder' => strtolower($date_pattern))
+            'widget' => 'single_text',
+            'format' => \IntlDateFormatter::LONG,
+            'pattern' => $date_pattern,
+            'inputmask' => true,
+            'placeholder' => true,
         ) + parent::getDefaultOptions($options);
     }
 }
